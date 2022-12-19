fa7593127855189f80d820417ec4c8c676c4a797
trankyy0212 <tranthuanky@gmail.com>
2022-12-19 22:30:03 +0700

    bt

--- module2/src/ss13_ThuatToanTimKiem/bai_tap/BinarySearchUsingRecursion.java
+++ module2/src/ss13_ThuatToanTimKiem/bai_tap/BinarySearchUsingRecursion.java
@@ -11,35 +11,38 @@ public class BinarySearchUsingRecursion {
         int arr[] = new int[length];
 
 
-        for (int i = 0; i <arr.length ; i++) {
-            System.out.println("Input arr["+i+"]");
-            arr[i]= Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Input arr[" + i + "]");
+            arr[i] = Integer.parseInt(sc.nextLine());
         }
 //        chuyen thanh mang
         System.out.println(Arrays.toString(arr));
-//        ắp xếp các phần tử theo thứ tự tăng dần
+//        sắp xếp các phần tử theo thứ tự tăng dần
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
-//        System.out.println("Input value need to find");
-//        int value = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Input value need to find");
+        int value = Integer.parseInt(sc.nextLine());
+        System.out.println("Input value need to find in index :"+binarySearch(arr,0,arr.length-1,value));
 
     }
 
     private static int binarySearch(int[] arr, int left, int right, int value) {
-        left=0;
-        right= arr.length-1;
-        while (left<=right){
-            int mid = (left+right)/2;
-            if (arr[mid] == value){
+        left = 0;
+        right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == value) {
                 return mid;
-            }else if (arr[mid]>value){
-                return binarySearch(arr,mid-1,right,value);
-            }else {
-                return binarySearch(arr,mid+1,right,value);
+            } else if (value > arr[mid]) {
+                return binarySearch(arr, mid + 1, right, value);
+            } else {
+                return binarySearch(arr, mid - 1, right, value);
             }
         }
-        return left-1;
+        return - 1;
     }
 
 }
