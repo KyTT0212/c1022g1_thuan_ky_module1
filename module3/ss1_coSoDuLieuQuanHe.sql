5af272631c663dcb4af6bb3a0d7970d80034f8ba
trankyy0212 <tranthuanky@gmail.com>
2023-01-10 11:36:28 +0700

    br

--- module3/ss1_coSoDuLieuQuanHe.sql
+++ module3/ss1_coSoDuLieuQuanHe.sql
@@ -1,11 +1,23 @@
-create table teacher(
+use student_management;
+
+create table student(
 id int,
 `name` varchar(50),
 age int,
 country varchar(50)
 );
 
+create table class(
+id int,
+`name` varchar(50)
+);
+
 insert into class (id,name) values (1,"C1022G1");
-select * from class;
 insert into class (id,name) values (2,"C1122G1");
-select * from class;
+
+create table teacher(
+id int,
+`name` varchar(50),
+age int,
+country varchar(50)
+);
